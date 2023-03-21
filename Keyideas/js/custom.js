98f4ae8d2243f8da0554731996641ce046378595
NEERAJ VERMA <99002607+neerajverma007@users.noreply.github.com>
2023-03-21 18:41:03 +0530

    Add files via upload

---
+++ Keyideas/js/custom.js
@@ -0,0 +1,57 @@
+const rects = document.querySelectorAll(".rCon")
+rects.forEach(rect => rect.addEventListener("click", rightClick));
+function rightClick() {
+  if (this.classList.contains("v")) return;
+  if (this.classList.contains("l")) {
+    const vrect = document.querySelector(".rCon.v")
+    const lrect = document.querySelector(".rCon.l")
+    const rrect = document.querySelector(".rCon.r")
+    const rAng = getAngle(rrect.style.transform);
+    const lAng = getAngle(lrect.style.transform);
+    const vAng = getAngle(vrect.style.transform);
+    rrect.style.height = "400px"
+    rrect.style.transform = `translateX(-50%) rotate(${rAng + 180}deg)`
+    lrect.style.height = "600px"
+    lrect.style.transform = `translateX(-50%) rotate(${lAng + 90}deg)`
+    vrect.style.height = "600px"
+    vrect.style.transform = `translateX(-50%) rotate(${vAng + 90}deg)`
+    rrect.classList.remove("r");
+    lrect.classList.remove("l");
+    vrect.classList.remove("v");
+    rrect.classList.add("l");
+    lrect.classList.add("v");
+    vrect.classList.add("r");
+  };
+  if (this.classList.contains("r")) {
+    const vrect = document.querySelector(".rCon.v")
+    const lrect = document.querySelector(".rCon.l")
+    const rrect = document.querySelector(".rCon.r")
+    const rAng = getAngle(rrect.style.transform);
+    const lAng = getAngle(lrect.style.transform);
+    const vAng = getAngle(vrect.style.transform);
+    console.log("R ", rAng, "L ", lAng, "V ", vAng)
+    rrect.style.height = "400px"
+    rrect.style.transform = `translateX(-50%) rotate(${rAng - 90}deg)`
+    lrect.style.height = "500px"
+    lrect.style.transform = `translateX(-50%) rotate(${lAng - 180}deg)`
+    vrect.style.height = "500px"
+    vrect.style.transform = `translateX(-50%) rotate(${vAng - 90}deg)`
+    setTimeout(() => {
+      rrect.classList.remove("r");
+      lrect.classList.remove("l");
+      vrect.classList.remove("v");
+      rrect.classList.add("v");
+      lrect.classList.add("r");
+      vrect.classList.add("l");
+    }, 1000);
+  }
+}
+function getAngle(str) {
+  const regex = /rotate\(([^)]+)\)/;
+  const match = regex.exec(str);
+  if (match) {
+    const value = match[1];
+    return parseInt(value);
+  }
+  return null
+}
